b817e34e1f6d30ef6c327093a1dfc5a297694c8c
etorresmxl-ux <etorres.mxl@gmail.com>
2026-02-13 15:48:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,25 +9,26 @@ st.set_page_config(page_title="Inmobiliaria - Drive", layout="wide")
 
 # --- CREDENCIALES DIRECTAS ---
 # He formateado tu JSON para que Streamlit lo reconozca directamente
-creds = {
-    "type": "service_account",
-    "project_id": "inmobiliaria-487222",
-    "private_key_id": "5e3fa80e51ca83ba8c1fcc4d56347400ec45816f",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCzCReqLm7+QMuJ\niUaoYbTN4rKrg+VzD62VbM7a5e9Zb1MNTzDDiIXbuAC127Q0DuKes4NoJf8vaKlg\n3ZLoRoxnqn7gKgN7UDs04wW7gtmGICXL1F8TxWfrIbyfHJY179K4E0f/u+KE8s/3\n8EKXpOpQFMHeZzrkJF99Jq9hQscO3K0fQADhZfTQA/EZkBqmfNQKLA8jEPH49hJH\nIK+QNFh3uxZtFYV1FBR0oP7b4R937ySVJ98WLjW79B78YtDz3GZNrzSU72XWy4Ti\njNFbZUBWxSlYThjAuYrUI+8cZhKhM1ShbYEC+usgIzlY6ejMVnf75pGbZjPxE5Ha\nsAF7FiaFAgMBAAECggEAEelEdVGc6BHaawGRUJIp0Pkvj8orv9WfM5ZFoY+kYOmq\nwOedxMoZPjCL3aXDwvuEP4VNDbPTck5Bt7+jDVrVfB+J/uolHAacTb+ymJ5QHcOE\nlH5EHsm+ij7/TFnDS1UZWzIOn26QDGXwWWkveFVW3bkd5h6kvSNIbFBc9lmEaZ+O\nLwAMz4F2QEdgOg0tW+5N80/tmWdQpIDJAUk2poe/doAhQ/i8RrXJv66LSOiGOPGI\ngDYNTF+D1ZaJQQgSaynRgQ2oxxlDIh56c4o0cbZS4Amu0JgJDlKqXfK8v3AorzLD\nYaG+bq43zs8gsWrx9M+U8MZAiYDQL/TuT6AY2JU0JwKBgQDp9lGB74PIcXHoiXUx\n9tuW3lBr9SD509MsxQZyWLUbo3vGglqQqnj2r7dVcDDJFqnI84eNE1kkCi5sQt5G\nEdA7W9TrCVhn2AZ+pWyzgFqVhrofRBf58bSPjCjjZS8QQ5BeVj2ScGjYHEGL/dlj\nv8Ml3qGm5EdVh7fp+Gq0xujbFwKBgQDD5kq1ZCiA1ZhHzKsCQacPstePhtjBUCq+\nic4ud7r6IVGnLdRYb9zAvsBubNkddAqs1fvci+No/tpC+YAI5GuHd6d+/PTADfkv\noEFTQNJ1Tn0ioKm1N2XxZfabQjMykpp8dQnwPCNcrEQ/KxeSgn2VEB961z26AsgX\n7AQdX71cwwKBgC6yvJgbz4j9o8fPT/YWGMRnQVQbDGbxMdBYzy2ZqSSIIBeCQ0Nm\n01ghwI0sJICuplr2yNKOzxcTdSqkuirwOUjvznLPXbb0dm9m42h8sRfxWUsOU17P\nMMQKLMsekiND6Rf7TLTi/PpNwYOIupBfYTs40bk3DUn0GfB4ZgwJO8cnAoGAFMcN\nL3YDEb8V2q+rh569AF5AnLl5re85yWHGW4lZbIQyK+AhgIvExzC0KkIjOQuAwloj\nz32Kzi0Rqz7ZRJgti322ZzKfJuuUfWeq5hCfAdAkV5LgzRamGldtM4Ru97My7XZg\nanmGaqLezjBc3K44caH3JMlFg8AdxuPCf/cSl48CgYAyNcVSp3Ms0ZWmOarRhur6\nqciXpd7+tkIPn9dAzK+QZD0HgJVAYEO5BmSRpQdBoETbNc3j/Y5iXWUJW/L7ly2Y\niv/exwZffbABCVqglOn6aqyyoY81YzsdYBPJxAs6AK+vXO/mZyiYn7TjOgF4/ZPQ\nklUH/gsZNA67tlQySVewtQ==\n-----END PRIVATE KEY-----\n",
-    "client_email": "inmobiliaria@inmobiliaria-487222.iam.gserviceaccount.com",
-    "client_id": "118429869319608246472",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/inmobiliaria%40inmobiliaria-487222.iam.gserviceaccount.com"
+creds = creds = {
+  "type": "service_account",
+  "project_id": "inmobiliaria-487222",
+  "private_key_id": "5e3fa80e51ca83ba8c1fcc4d56347400ec45816f",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCzCReqLm7+QMuJ\niUaoYbTN4rKrg+VzD62VbM7a5e9Zb1MNTzDDiIXbuAC127Q0DuKes4NoJf8vaKlg\n3ZLoRoxnqn7gKgN7UDs04wW7gtmGICXL1F8TxWfrIbyfHJY179K4E0f/u+KE8s/3\n8EKXpOpQFMHeZzrkJF99Jq9hQscO3K0fQADhZfTQA/EZkBqmfNQKLA8jEPH49hJH\nIK+QNFh3uxZtFYV1FBR0oP7b4R937ySVJ98WLjW79B78YtDz3GZNrzSU72XWy4Ti\njNFbZUBWxSlYThjAuYrUI+8cZhKhM1ShbYEC+usgIzlY6ejMVnf75pGbZjPxE5Ha\nsAF7FiaFAgMBAAECggEAEelEdVGc6BHaawGRUJIp0Pkvj8orv9WfM5ZFoY+kYOmq\nwOedxMoZPjCL3aXDwvuEP4VNDbPTck5Bt7+jDVrVfB+J/uolHAacTb+ymJ5QHcOE\nlH5EHsm+ij7/TFnDS1UZWzIOn26QDGXwWWkveFVW3bkd5h6kvSNIbFBc9lmEaZ+O\nLwAMz4F2QEdgOg0tW+5N80/tmWdQpIDJAUk2poe/doAhQ/i8RrXJv66LSOiGOPGI\ngDYNTF+D1ZaJQQgSaynRgQ2oxxlDIh56c4o0cbZS4Amu0JgJDlKqXfK8v3AorzLD\nYaG+bq43zs8gsWrx9M+U8MZAiYDQL/TuT6AY2JU0JwKBgQDp9lGB74PIcXHoiXUx\n9tuW3lBr9SD509MsxQZyWLUbo3vGglqQqnj2r7dVcDDJFqnI84eNE1kkCi5sQt5G\nEdA7W9TrCVhn2AZ+pWyzgFqVhrofRBf58bSPjCjjZS8QQ5BeVj2ScGjYHEGL/dlj\nv8Ml3qGm5EdVh7fp+Gq0xujbFwKBgQDD5kq1ZCiA1ZhHzKsCQacPstePhtjBUCq+\nic4ud7r6IVGnLdRYb9zAvsBubNkddAqs1fvci+No/tpC+YAI5GuHd6d+/PTADfkv\noEFTQNJ1Tn0ioKm1N2XxZfabQjMykpp8dQnwPCNcrEQ/KxeSgn2VEB961z26AsgX\n7AQdX71cwwKBgC6yvJgbz4j9o8fPT/YWGMRnQVQbDGbxMdBYzy2ZqSSIIBeCQ0Nm\n01ghwI0sJICuplr2yNKOzxcTdSqkuirwOUjvznLPXbb0dm9m42h8sRfxWUsOU17P\nMMQKLMsekiND6Rf7TLTi/PpNwYOIupBfYTs40bk3DUn0GfB4ZgwJO8cnAoGAFMcN\nL3YDEb8V2q+rh569AF5AnLl5re85yWHGW4lZbIQyK+AhgIvExzC0KkIjOQuAwloj\nz32Kzi0Rqz7ZRJgti322ZzKfJuuUfWeq5hCfAdAkV5LgzRamGldtM4Ru97My7XZg\nanmGaqLezjBc3K44caH3JMlFg8AdxuPCf/cSl48CgYAyNcVSp3Ms0ZWmOarRhur6\nqciXpd7+tkIPn9dAzK+QZD0HgJVAYEO5BmSRpQdBoETbNc3j/Y5iXWUJW/L7ly2Y\niv/exwZffbABCVqglOn6aqyyoY81YzsdYBPJxAs6AK+vXO/mZyiYn7TjOgF4/ZPQ\nklUH/gsZNA67tlQySVewtQ==\n-----END PRIVATE KEY-----\n",
+  "client_email": "inmobiliaria@inmobiliaria-487222.iam.gserviceaccount.com",
+  "client_id": "118429869319608246472",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/inmobiliaria%40inmobiliaria-487222.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
 }
 
 URL_SHEET = "https://docs.google.com/spreadsheets/d/1TIeJ2fjJ6WHn124b8iL9LgTNuRZ_50YyekSad0uK1jE/"
 
-# --- CONEXIÓN ---
+# --- CONEXIÓN CORREGIDA ---
 try:
-    # Intentamos conectar usando las credenciales inyectadas directamente
-    conn = st.connection("gsheets", type=GSheetsConnection, **creds)
+    # Eliminamos el type="gsheets" de afuera porque ya viene dentro de tus 'creds'
+    conn = st.connection("gsheets", **creds)
 except Exception as e:
     st.error(f"Error de conexión con Google Drive: {e}")
     st.stop()
@@ -123,3 +124,4 @@ elif choice == "Directorio":
             n_df = pd.concat([df_c, pd.DataFrame([{"nombre": nom}])], ignore_index=True)
             guardar(n_df, "clientes")
             st.rerun()
+
